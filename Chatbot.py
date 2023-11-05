d2601dc4c2c2a23a038c2bb035c27bc4a6c4e0b1
MagicFitz <71171268+MagicFitz@users.noreply.github.com>
2023-11-05 18:27:32 +0800

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,14 +1,14 @@
 import openai
 import streamlit as st
 
-with st.sidebar:
+'''with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-
-st.title("💬 Chatbot")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+'''
+st.title("💬 藏格矿业知识问答")
+st.caption("回答关于藏格矿业各种规章制度的问题")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
@@ -19,8 +19,8 @@ if prompt := st.chat_input():
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
-
-    openai.api_key = openai_api_key
+    openai.api_base = 'https://api.openai-proxy.org/v1'
+    openai.api_key = 'sk-yE9AUncq99czee9GsytQYP4QxYuZal44kPMx5y9rkqojGVu3'
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
