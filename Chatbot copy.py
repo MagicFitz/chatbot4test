346eb7c5773d7d0b03f6237294b054ec4225ce88
MagicFitz <71171268+MagicFitz@users.noreply.github.com>
2023-11-08 20:25:03 +0800

    change

--- Chatbot copy.py
+++ Chatbot copy.py
@@ -1,6 +1,9 @@
 import openai
 import streamlit as st
-
+import FAISS
+import os
+import json
+import pandas as pd
 '''with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
